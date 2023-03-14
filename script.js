f312dcafc9ba4184cef959d4d6e0f458167f6c14
Davincii254 <victorkuria7@gmail.com>
2023-03-14 08:28:30 +0300

    commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// Arrow function
+// const sayHello = (name) => {
+//   console.log(`Hello, ${name}!`);
+// };
+
+// sayHello('John'); // Output: "Hello, John!"
+
+
+// Callback Function
+// const sayHello = (name) => {
+//   console.log(`Hello, ${name}!`);
+// };
+
+// sayHello('John'); // Output: "Hello, John!"
+
+
+// Callback Functions
+// function add(num1, num2, callback) {
+//   const sum = num1 + num2;
+//   callback(sum);
+// }
+
+// function logResult(result) {
+//   console.log(`The result is ${result}.`);
+// }
+
+// add(2, 3, logResult); // Output: "The result is 5."
+
+
+
