5ec668b7b6dddadc4ee2fc47f34e77917a8a577c
Davincii254 <victorkuria7@gmail.com>
2023-03-14 08:58:49 +0300

    Uncomment

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-// Arrow function
-// const sayHello = (name) => {
-//   console.log(`Hello, ${name}!`);
-// };
+Arrow function
+const sayHello = (name) => {
+  console.log(`Hello, ${name}!`);
+};
 
-// sayHello('John'); // Output: "Hello, John!"
-
-
-// Callback Function
-// const sayHello = (name) => {
-//   console.log(`Hello, ${name}!`);
-// };
-
-// sayHello('John'); // Output: "Hello, John!"
+sayHello('John'); // Output: "Hello, John!"
 
 
-// Callback Functions
-// function add(num1, num2, callback) {
-//   const sum = num1 + num2;
-//   callback(sum);
-// }
+Callback Function
+const sayHello = (name) => {
+  console.log(`Hello, ${name}!`);
+};
 
-// function logResult(result) {
-//   console.log(`The result is ${result}.`);
-// }
+sayHello('John'); // Output: "Hello, John!"
 
-// add(2, 3, logResult); // Output: "The result is 5."
+
+Callback Functions
+function add(num1, num2, callback) {
+  const sum = num1 + num2;
+  callback(sum);
+}
+
+function logResult(result) {
+  console.log(`The result is ${result}.`);
+}
+
+add(2, 3, logResult); // Output: "The result is 5."
 
 
 
